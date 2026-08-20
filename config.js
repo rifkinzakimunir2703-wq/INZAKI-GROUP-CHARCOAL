3053df714b4597ce08df1ba61b0769522dca0d92
rifkinzakimunir2703-wq <rifkinzakimunir2703@gmail.com>
2026-08-21 02:56:28 +0700

    config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
    KONFIGURASI SUPABASE — INZAKI GROUP Charcoal Business Portal
    ========================================================================= */
 
-const SUPABASE_URL = "https://gtxkwmlxzxnuqumcumpa.supabase.co";
+const NEXT_PUBLIC_SUPABASE_URL=https://gtxwkmlxxznuqumcumpa.supabase.co
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZXMiLCJyZWYiOiJndHhrd21seHpudXF1cW1jdW1wYSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzg2OTY0ODU4LCJleHAiOjIxMDI1NDA4NTh9.N-EignTmMMa_Qz3t4OvedVd0ncirxJfoX7wFeADcXz0";
